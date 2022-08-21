07088f7f8f64f89bdcc5dec4b2de68523e1b15c0
Tsukasa Nishiyama <nsymtks@gmail.com>
2022-08-21 16:30:04 +0900

    nvimのgitとlspのキーバインドを調整

--- config/nvim/after/plugin/git.rc.lua
+++ config/nvim/after/plugin/git.rc.lua
@@ -5,7 +5,13 @@ git.setup {
   keymaps = {
     -- Open blame window
     blame = 'gb',
+    -- Close lame window
+    quit_blame = 'q',
     -- Open file/folder in git repository
-    browse = 'go'
+    browse = 'go',
+    -- Open a new diff that compares against the current index
+    diff = 'gd',
+    -- Close diff window
+    diff_close = 'q',
   }
 }
