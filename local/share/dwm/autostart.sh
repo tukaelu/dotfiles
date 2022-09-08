385deef406c820ab68f33d392ce1e3f8fd5ccb99
Tsukasa Nishiyama <nsymtks@gmail.com>
2022-09-08 09:34:41 +0900

    起動時に.xbindkeysを読み込むように

--- local/share/dwm/autostart.sh
+++ local/share/dwm/autostart.sh
@@ -4,4 +4,6 @@ pgrep -x picom > /dev/null || picom -b
 pgrep -x fcitx > /dev/null || fcitx &
 pgrep -x ulauncher > /dev/null || ulauncher &
 
+xbindkeys -p
+
 /usr/bin/feh --randomize --bg-fill /home/tuka/Wallpaper > /dev/null 2>&1
