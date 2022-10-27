0466a4a06b10acb2a19e8d69e7c9bf058da37e67
Tsukasa Nishiyama <nsymtks@gmail.com>
2022-10-27 21:00:26 +0900

    スクリーンロックを追加

--- local/share/dwm/autostart.sh
+++ local/share/dwm/autostart.sh
@@ -4,6 +4,8 @@ pgrep -x picom > /dev/null || picom -b
 pgrep -x fcitx > /dev/null || fcitx &
 pgrep -x ulauncher > /dev/null || ulauncher --hide-window &
 
+/usr/bin/feh --randomize --bg-fill /home/tuka/Pictures/Wallpaper > /dev/null 2>&1
+
 xbindkeys -p
 
-/usr/bin/feh --randomize --bg-fill /home/tuka/Pictures/Wallpaper > /dev/null 2>&1
+xautolock -time 25 -locker slock &
