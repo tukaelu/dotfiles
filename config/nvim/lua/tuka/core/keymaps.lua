be1e450b22ed526043e9525663e140727a9e2d51
Tsukasa Nishiyama <nsymtks@gmail.com>
2022-11-13 11:50:31 +0900

    キーマップを変更

--- config/nvim/lua/tuka/core/keymaps.lua
+++ config/nvim/lua/tuka/core/keymaps.lua
@@ -1,61 +1,57 @@
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
 -- set leader key to space
 vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-local keymap = vim.keymap
-
-keymap.set('i', '<leader>jk', '<ESC>') -- Exit insert mode
-keymap.set('n', '<leader>q', ':q<CR>') -- Quit
-keymap.set('n', '<leader>nh', ':nohl<CR>') -- clear search highlights
+keymap('i', 'jk', '<ESC>', opts) -- Exit insert mode
+keymap('n', '<leader>q', ':q<CR>', opts) -- Quit
+keymap('n', '<leader>nh', ':nohl<CR>', opts) -- clear search highlights
 
 -- Explorer
-keymap.set('n', '<leader>e', '<Cmd>Telescope file_browser<CR>')
+keymap('n', '<leader>e', '<Cmd>Telescope file_browser<CR>', opts)
 
 -- Telescope
-keymap.set('n', '<leader>f', '<Cmd>Telescope find_files hidden=true<CR>')
-keymap.set('n', '<leader>F', '<Cmd>Telescope find_files hidden=true no_ignore=true<CR>')
-keymap.set('n', '<leader>g', '<Cmd>Telescope live_grep<CR>')
-keymap.set('n', '<leader>b', '<Cmd>Telescope buffers<CR>')
-keymap.set('n', '<leader>?', '<Cmd>Telescope help_tags<CR>')
-keymap.set('n', '<leader>;', '<Cmd>Telescope resume<CR>')
+keymap('n', '<leader>f', '<Cmd>Telescope find_files hidden=true<CR>', opts)
+keymap('n', '<leader>F', '<Cmd>Telescope find_files hidden=true no_ignore=true<CR>', opts)
+keymap('n', '<leader>g', '<Cmd>Telescope live_grep<CR>', opts)
+keymap('n', '<leader>B', '<Cmd>Telescope buffers<CR>', opts)
+keymap('n', '<leader>?', '<Cmd>Telescope help_tags<CR>', opts)
+keymap('n', '<leader>;', '<Cmd>Telescope resume<CR>', opts)
 
 -- Git
-keymap.set('n', '<leader>h', '<Cmd>Git blame<CR>')
-keymap.set('n', '<leader>d', '<Cmd>Gdiffsplit<CR>')
-keymap.set('n', '<leader>o', "<Cmd>'<,'>GBrowse<CR>")
+keymap('n', '<leader>b', '<Cmd>Git blame<CR>', opts)
+keymap('n', '<leader>d', '<Cmd>Gdiffsplit<CR>', opts)
+keymap('n', '<leader>o', "<Cmd>'<,'>GBrowse<CR>", opts)
 
 -- LspSaga
-local opts = { noremap = true, silent = true }
-keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
-keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
-keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-keymap.set('n', '<leader>l', '<Cmd>Lspsaga lsp_finder<CR>', opts)
-keymap.set('n', '<leader>p', '<Cmd>Lspsaga peek_definition<CR>', opts)
-keymap.set('n', '<leader>r', '<Cmd>Lspsaga rename<CR>', opts)
+keymap('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
+keymap('n', '<C-l>', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+keymap('n', '<C-d>', '<Cmd>Lspsaga peek_definition<CR>', opts)
+keymap('n', '<C-r>', '<Cmd>Lspsaga rename<CR>', opts)
 
 -- Editor
-keymap.set('n', '<C-a>', 'gg<S-v>G') -- All selections
-keymap.set('i', '<C-s>', '<C-o>:update<CR>', { silent = true }) -- Save (in insert mode)
-keymap.set('n', '<C-s>', ':update<CR>', { silent = true }) -- Save
-keymap.set('i', '<C-Space>', '<C-x><C-o>') -- Omnifunc
-keymap.set('n', '+', '<C-a>') -- increment number
-keymap.set('n', '-', '<C-x>') -- decrement number
+keymap('n', '<C-a>', 'gg<S-v>G', opts) -- All selections
+keymap('i', '<C-s>', '<C-o>:update<CR>', opts) -- Save (in insert mode)
+keymap('n', '<C-s>', ':update<CR>', opts) -- Save
+keymap('i', '<C-Space>', '<C-x><C-o>', opts) -- Omnifunc
+keymap('n', '+', '<C-a>', opts) -- increment number
+keymap('n', '-', '<C-x>', opts) -- decrement number
+keymap('v', '>', '>gv', opts) -- stay in visual indent
+keymap('v', '<', '<gv', opts) -- stay in visual indent
 
 -- Tab / Window
-keymap.set('n', 'T', ':$tabnew<CR>', { silent = true }) -- Open new tab
-keymap.set('n', '<', ':-tabnext<CR>', { silent = true }) -- Move tab next
-keymap.set('n', '>', ':+tabnext<CR>', { silent = true }) -- Move tab prev
-
-keymap.set('n', '_', ':sp<CR>', { silent = true }) -- Split window horizontally
-keymap.set('n', '<bar>', ':vsp<CR>', { silent = true }) -- Split window vertically
-keymap.set('n', 'wh', '<C-w>h') -- Move window left
-keymap.set('n', 'w<left>', '<C-w>h')
-keymap.set('n', 'wk', '<C-w>k') -- Move window up
-keymap.set('n', 'w<up>', '<C-w>k')
-keymap.set('n', 'wj', '<C-w>j') -- Move window down
-keymap.set('n', 'w<down>', '<C-w>j')
-keymap.set('n', 'wl', '<C-w>l') -- Move window right
-keymap.set('n', 'w<right>', '<C-w>l')
+keymap('n', 'T', ':$tabnew<CR>', opts) -- Open new tab
+keymap('n', '<', ':-tabnext<CR>', opts) -- Move tab next
+keymap('n', '>', ':+tabnext<CR>', opts) -- Move tab prev
+keymap('n', '_', ':sp<CR>', opts) -- Split window horizontally
+keymap('n', '<bar>', ':vsp<CR>', opts) -- Split window vertically
+keymap('n', '<C-Up>', ':resize -2<CR>', opts) -- Window resize
+keymap('n', '<C-Down>', ':resize +2<CR>', opts) -- Window resize
+keymap('n', '<C-Right>', ':vertical resize -2<CR>', opts) -- Window resize
+keymap('n', '<C-Left>', ':vertical resize +2<CR>', opts) -- Window resize
 
 -- Bufferline
-keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
-keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
+keymap('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', opts)
+keymap('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', opts)
