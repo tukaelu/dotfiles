9a2b1dcfcb510c0d948ae9dc0d8dba113698182e
Tsukasa Nishiyama <tukaelu@hatena.ne.jp>
2022-11-23 21:27:20 +0900

    [lspsaga]リネームのキーバインドを変更

--- config/nvim/lua/tuka/core/keymaps.lua
+++ config/nvim/lua/tuka/core/keymaps.lua
@@ -29,7 +29,7 @@ keymap('n', '<leader>o', "<Cmd>'<,'>GBrowse<CR>", opts)
 keymap('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 keymap('n', '<C-l>', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 keymap('n', '<C-d>', '<Cmd>Lspsaga peek_definition<CR>', opts)
-keymap('n', '<C-r>', '<Cmd>Lspsaga rename<CR>', opts)
+keymap('n', '<C-_>', '<Cmd>Lspsaga rename<CR>', opts)
 
 -- Editor
 keymap('n', '<C-a>', 'gg<S-v>G', opts) -- All selections
