be8fd0a10fc65f616b2059d9b7394c8ba50f27c3
Tsukasa Nishiyama <nsymtks@gmail.com>
2023-03-18 14:22:24 +0900

    [nvim] Kanagawa.nvimのアップデートに合わせて修正

--- config/nvim/lua/tuka/core/colorscheme.lua
+++ config/nvim/lua/tuka/core/colorscheme.lua
@@ -3,22 +3,33 @@ vim.g.nord_disable_background = true
 
 -- kanagawa
 require('kanagawa').setup({
+  compile = false,
   undercurl = true, -- enable undercurls
   commentStyle = { italic = true },
   functionStyle = {},
   keywordStyle = { italic = true },
   statementStyle = { bold = true },
   typeStyle = {},
-  variablebuiltinStyle = { italic = true },
-  specialReturn = true, -- special highlight for the return keyword
-  specialException = true, -- special highlight for exception handling keywords
-  transparent = false, -- do not set background color
-  dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-  globalStatus = false, -- adjust window separators highlight for laststatus=3
+  transparent = false,   -- do not set background color
+  dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
   terminalColors = true, -- define vim.g.terminal_color_{0,17}
-  colors = {},
-  overrides = {},
-  theme = "default" -- Load "default" theme or the experimental "light" theme
+  colors = {
+    palette = {},
+    theme = {
+      wave = {},
+      lotus = {},
+      dragon = {},
+      all = {}
+    }
+  },
+  overrides = function(colors)
+    return {}
+  end,
+  theme = 'wave',
+  background = {
+    dark = 'wave',
+    light = 'lotus'
+  }
 })
 
 local status, _ = pcall(vim.cmd, 'colorscheme kanagawa')
